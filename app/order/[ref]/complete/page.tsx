2ee9c5e0f53e9f2e864a52f433ebb9d9ed21989b
Izuchukwu Promise <izuchukwupromise01@gmail.com>
2026-01-03 06:58:34 +0100

    enabled paystack by uncommenting href

--- app/order/[ref]/complete/page.tsx
+++ app/order/[ref]/complete/page.tsx
@@ -95,8 +95,7 @@ export default async function OrderCompletePage({
       {/* PAYSTACK OPTIONAL */}
       {order.payment_status !== 'paid' && (
         <Link
-          // href={`/pay/${order.order_ref}`}
-          href={`#`}
+          href={`/pay/${order.order_ref}`}
           className="block text-center border border-black py-3 rounded-lg bg-(--color-navyBlue) text-white hover:bg-(--color-navyBlue)/80 duration-200"
         >
           Pay Now with Card/Paystack
